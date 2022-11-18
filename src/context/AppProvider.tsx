35881764246ec5a40c0b819d4d08230fcc1b13c7
Jonathan Gan <jonny2298@live.com>
2022-11-18 11:44:46 -0500

    updated and removed some buggy libraries not needed

--- src/context/AppProvider.tsx
+++ src/context/AppProvider.tsx
@@ -10,12 +10,13 @@ interface AppContextInterface {
 export const AppContext = createContext<AppContextInterface | any>({});
 
 export const AppProvider = (props: { children: any }) => {
-    const [currentWalletAddress, setCurrentWalletAddress] = useState<string>("")
-    const [key, setKey] = useState<string>("")
-    const [email, setEmail] = useState<string>("")
+    const [currentWalletAddress, setCurrentWalletAddress] = useState<string>("");
+    const [key, setKey] = useState<string>("");
+    const [email, setEmail] = useState<string>("");
     const [userInfo, setUserInfo] = useState({});
     const [favors, setFavors] = useState([]);
 
+    
     return (
         <AppContext.Provider value={{ currentWalletAddress, setCurrentWalletAddress, key, setKey, email, setEmail, userInfo, setUserInfo, favors, setFavors }}>
             {props.children}
